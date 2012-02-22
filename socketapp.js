bbb3d928014cf3c7db139de3ff8e93cb5ebecaca
Jonathan Mumm <mumm@tokbox.com>
2012-02-22 12:58:20 -0800

    Added exception handling

--- socketapp.js
+++ socketapp.js
@@ -28,84 +28,88 @@ exports.start = function(sockets) {
 
     socket.on('message', function (msg) {
       console.log(msg);
-      msg = JSON.parse(msg);
-      switch (msg.event) {
-        case "next":
-          // Create a "user" data object for me
-          var me = {
-            sessionId: msg.args.sessionId,
-            socketId: socket.id
-          };
+      try {
+        msg = JSON.parse(msg);
+        switch (msg.event) {
+          case "next":
+            // Create a "user" data object for me
+            var me = {
+              sessionId: msg.args.sessionId,
+              socketId: socket.id
+            };
 
-          var partner;
-          var partnerSocket;
-          // Look for a user to partner with in the list of solo users
-          for (var i = 0; i < soloUsers.length; i++) {
-            var tmpUser = soloUsers[i];
+            var partner;
+            var partnerSocket;
+            // Look for a user to partner with in the list of solo users
+            for (var i = 0; i < soloUsers.length; i++) {
+              var tmpUser = soloUsers[i];
 
-            // Make sure our last partner is not our new partner
-            if (socket.partner != tmpUser) {							
-              // Get the socket client for this user
-              partnerSocket = clients[tmpUser.socketId];
+              // Make sure our last partner is not our new partner
+              if (socket.partner != tmpUser) {							
+                // Get the socket client for this user
+                partnerSocket = clients[tmpUser.socketId];
 
-              // Remove the partner we found from the list of solo users
-              soloUsers.splice(i, 1);
+                // Remove the partner we found from the list of solo users
+                soloUsers.splice(i, 1);
 
-              // If the user we found exists...
-              if (partnerSocket) {
-                // Set as our partner and quit the loop today
-                partner = tmpUser;
-                break;
+                // If the user we found exists...
+                if (partnerSocket) {
+                  // Set as our partner and quit the loop today
+                  partner = tmpUser;
+                  break;
+                }
               }
             }
-          }
 
-          // If we found a partner...
-          if (partner) {
+            // If we found a partner...
+            if (partner) {
 
-            // Tell myself to subscribe to my partner
-            socket.emit('subscribe', {
-              sessionId: partner.sessionId,
-              token: ot.generateToken({ 
+              // Tell myself to subscribe to my partner
+              socket.emit('subscribe', {
                 sessionId: partner.sessionId,
-                role: opentok.Roles.SUBSCRIBER
-              })
-            });
+                token: ot.generateToken({ 
+                  sessionId: partner.sessionId,
+                  role: opentok.Roles.SUBSCRIBER
+                })
+              });
 
-            // Tell my partner to subscribe to me
-            partnerSocket.emit('subscribe', {
-              sessionId: me.sessionId,
-              token: ot.generateToken({ 
+              // Tell my partner to subscribe to me
+              partnerSocket.emit('subscribe', {
                 sessionId: me.sessionId,
-                role: opentok.Roles.SUBSCRIBER
-              })
-            });
+                token: ot.generateToken({ 
+                  sessionId: me.sessionId,
+                  role: opentok.Roles.SUBSCRIBER
+                })
+              });
 
-            // Mark that my new partner and me are partners
-            socket.partner = partner;
-            partnerSocket.partner = me;
+              // Mark that my new partner and me are partners
+              socket.partner = partner;
+              partnerSocket.partner = me;
 
-            // Mark that we are not in the list of solo users anymore
-            socket.inlist = false;
-            partnerSocket.inlist = false;
+              // Mark that we are not in the list of solo users anymore
+              socket.inlist = false;
+              partnerSocket.inlist = false;
 
-          } else {
+            } else {
 
-            // delete that i had a partner if i had one				
-            if (socket.partner) {							
-              delete socket.partner;
-            }
+              // delete that i had a partner if i had one				
+              if (socket.partner) {							
+                delete socket.partner;
+              }
 
-            // add myself to list of solo users if i'm not in the list
-            if (!socket.inlist) {
-              socket.inlist = true;
-              soloUsers.push(me);
-            }
+              // add myself to list of solo users if i'm not in the list
+              if (!socket.inlist) {
+                socket.inlist = true;
+                soloUsers.push(me);
+              }
 
-            // tell myself that there is nobody to chat with right now
-            socket.emit('empty');
-          }	
-        break;
+              // tell myself that there is nobody to chat with right now
+              socket.emit('empty');
+            }	
+          break;
+        }
+      } catch (err) {
+        console.log(err);
       }
     });
 
